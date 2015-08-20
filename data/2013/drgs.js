0cbcdecfc07515c156ecf01fd2ee6d986c2eef17
Robert Stewart <robert@wombatnation.com>
2015-08-20 16:30:02 -0700

    added 5 new DRGs for 2013 and changed to a js file so it can be more easily loaded by new Angular app

---
+++ data/2013/drgs.js
@@ -0,0 +1,473 @@
+var diagnosisFamilies = [
+  {
+    "name": "Diabetes",
+    "DRGs": [
+      {
+        "code": "641",
+        "name": "Other nutritional conditions"
+      },
+      {
+        "code": "640",
+        "name": "Other nutritional conditions (with major complications)"
+      },
+      {
+        "code": "638",
+        "name": "Diabetes (with complications)"
+      },
+      {
+        "code": "637",
+        "name": "Diabetes (with major complications)"
+      }
+    ]
+  },
+  {
+    "name": "Muscle and joints",
+    "DRGs": [
+      {
+        "code": "491",
+        "name": "Back & neck procedures"
+      },
+      {
+        "code": "536",
+        "name": "Hip fracture"
+      },
+      {
+        "code": "552",
+        "name": "Back pain"
+      },
+      {
+        "code": "469",
+        "name": "Joint replacement (with major complications)"
+      },
+      {
+        "code": "473",
+        "name": "Cervical spinal fusion"
+      },
+      {
+        "code": "563",
+        "name": "Other joint injuries"
+      },
+      {
+        "code": "460",
+        "name": "Spinal fusion"
+      },
+      {
+        "code": "470",
+        "name": "Joint replacement"
+      },
+      {
+        "code": "480",
+        "name": "Hip & femur procedures (with major complications)"
+      },
+      {
+        "code": "482",
+        "name": "Hip & femur procedures"
+      },
+      {
+        "code": "481",
+        "name": "Hip & femur procedures (with complications)"
+      },
+      {
+        "code": "467",
+        "name": "Revision of hip or knee replacement (with complications)"
+      }
+    ]
+  },
+  {
+    "name": "Skin",
+    "DRGs": [
+      {
+        "code": "603",
+        "name": "Cellulitis"
+      },
+      {
+        "code": "602",
+        "name": "Cellulitis (with major complications)"
+      }
+    ]
+  },
+  {
+    "name": "Blood",
+    "DRGs": [
+      {
+        "code": "812",
+        "name": "Blood disorder"
+      },
+      {
+        "code": "811",
+        "name": "Blood disorder (with major complications)"
+      }
+    ]
+  },
+  {
+    "name": "Brain",
+    "DRGs": [
+      {
+        "code": "65",
+        "name": "Stroke (with complications)"
+      },
+      {
+        "code": "69",
+        "name": "Ischemia"
+      },
+      {
+        "code": "101",
+        "name": "Seizures"
+      },
+      {
+        "code": "101",
+        "name": "Seizures (with major complications)"
+      },
+      {
+        "code": "57",
+        "name": "Degenerative nervous system disorders"
+      },
+      {
+        "code": "39",
+        "name": "Extracranial procedures"
+      },
+      {
+        "code": "64",
+        "name": "Stroke (with major complications)"
+      },
+      {
+        "code": "66",
+        "name": "Stroke"
+      },
+      {
+        "code": "74",
+        "name": "Cranial & peripheral nerve disorders"
+      }
+    ]
+  },
+  {
+    "name": "Digestive",
+    "DRGs": [
+      {
+        "code": "329",
+        "name": "Major small & large bowel procedures (with major complications)"
+      },
+      {
+        "code": "394",
+        "name": "Other digestive system diagnoses"
+      },
+      {
+        "code": "391",
+        "name": "Esophagitis, gastroenteritis, and other digestive disorders (with major complications)"
+      },
+      {
+        "code": "372",
+        "name": "Major gastrointestinal disorders (with complications)"
+      },
+      {
+        "code": "330",
+        "name": "Major small & large bowel procedures (with complications)"
+      },
+      {
+        "code": "331",
+        "name": "Major small & large bowel procedures"
+      },
+      {
+        "code": "392",
+        "name": "Esophagitis, gastroenteritis, and other digestive disorders"
+      },
+      {
+        "code": "390",
+        "name": "Intestinal obstruction"
+      },
+      {
+        "code": "378",
+        "name": "Gastrointestinal bleeding (with complications)"
+      },
+      {
+        "code": "377",
+        "name": "Gastrointestinal bleeding (with major complications)"
+      },
+      {
+        "code": "379",
+        "name": "Gastrointestinal bleeding"
+      },
+      {
+        "code": "389",
+        "name": "Intestinal obstruction (with complications)"
+      }
+    ]
+  },
+  {
+    "name": "Heart",
+    "DRGs": [
+      {
+        "code": "315",
+        "name": "Other circulatory diagnoses (with complications)"
+      },
+      {
+        "code": "313",
+        "name": "Chest pain"
+      },
+      {
+        "code": "309",
+        "name": "Arrhythmia (with complications)"
+      },
+      {
+        "code": "305",
+        "name": "Hypertension"
+      },
+      {
+        "code": "303",
+        "name": "Atherosclerosis"
+      },
+      {
+        "code": "301",
+        "name": "Peripheral vascular disease"
+      },
+      {
+        "code": "293",
+        "name": "Heart failure"
+      },
+      {
+        "code": "291",
+        "name": "Heart failure (with major complications)"
+      },
+      {
+        "code": "287",
+        "name": "Other heart conditions"
+      },
+      {
+        "code": "281",
+        "name": "Heart attack (with complications)"
+      },
+      {
+        "code": "244",
+        "name": "Pacemaker implant"
+      },
+      {
+        "code": "314",
+        "name": "Other circulatory diagnoses (with major complications)"
+      },
+      {
+        "code": "312",
+        "name": "Syncope"
+      },
+      {
+        "code": "310",
+        "name": "Arrhythmia"
+      },
+      {
+        "code": "308",
+        "name": "Arrhythmia (with major complications)"
+      },
+      {
+        "code": "300",
+        "name": "Peripheral vascular disease (with complications)"
+      },
+      {
+        "code": "292",
+        "name": "Heart failure (with complications)"
+      },
+      {
+        "code": "286",
+        "name": "Other heart conditions (with major complications)"
+      },
+      {
+        "code": "282",
+        "name": "Heart attack"
+      },
+      {
+        "code": "280",
+        "name": "Heart attack (with major complications)"
+      },
+      {
+        "code": "243",
+        "name": "Pacemaker implant (with complications)"
+      },
+      {
+        "code": "238",
+        "name": "Major cardiovascular procedures without major complications"
+      },
+      {
+        "code": "246",
+        "name": "Perc cardiovasc procedures with drug-eluting stent with major complications or 4+ vessels/stents"
+      },
+      {
+        "code": "247",
+        "name": "Perc cardiovasc procedures with drug-eluting stent without major complications"
+      },
+      {
+        "code": "249",
+        "name": "Perc cardiovasc procedures with non-drug-eluting stent without major complications"
+      },
+      {
+        "code": "251",
+        "name": "Perc cardiovasc procedures without coronary artery stent without major complications"
+      },
+      {
+        "code": "252",
+        "name": "Other vascular procedures with major complications"
+      },
+      {
+        "code": "253",
+        "name": "Other vascular procedures with complications"
+      },
+      {
+        "code": "254",
+        "name": "Other vascular procedures"
+      }
+    ]
+  },
+  {
+    "name": "Infections",
+    "DRGs": [
+      {
+        "code": "871",
+        "name": "Sepsis (with major complications)"
+      },
+      {
+        "code": "872",
+        "name": "Sepsis"
+      },
+      {
+        "code": "870",
+        "name": "Sepsis (with mechanical ventilation)"
+      },
+      {
+        "code": "853",
+        "name": "Parasitic disease"
+      }
+    ]
+  },
+  {
+    "name": "Kidney",
+    "DRGs": [
+      {
+        "code": "689",
+        "name": "Urinary tract infection (with major complications)"
+      },
+      {
+        "code": "683",
+        "name": "Kidney failure (with complications)"
+      },
+      {
+        "code": "698",
+        "name": "Other diagnoses (with major complications)"
+      },
+      {
+        "code": "699",
+        "name": "Other diagnoses (with complications)"
+      },
+      {
+        "code": "690",
+        "name": "Urinary tract infection"
+      },
+      {
+        "code": "684",
+        "name": "Kidney failure"
+      },
+      {
+        "code": "682",
+        "name": "Kidney failure (with major complications)"
+      }
+    ]
+  },
+  {
+    "name": "Lung",
+    "DRGs": [
+      {
+        "code": "189",
+        "name": "Respiratory failure"
+      },
+      {
+        "code": "177",
+        "name": "Respiratory infection (with major complications)"
+      },
+      {
+        "code": "176",
+        "name": "Pulmonary embolism"
+      },
+      {
+        "code": "178",
+        "name": "Respiratory infection (with complications)"
+      },
+      {
+        "code": "190",
+        "name": "COPD (with major complications)"
+      },
+      {
+        "code": "192",
+        "name": "COPD"
+      },
+      {
+        "code": "194",
+        "name": "Pneumonia (with complications)"
+      },
+      {
+        "code": "202",
+        "name": "Bronchitis/asthma (with complications)"
+      },
+      {
+        "code": "203",
+        "name": "Bronchitis/asthma"
+      },
+      {
+        "code": "195",
+        "name": "Pneumonia"
+      },
+      {
+        "code": "193",
+        "name": "Pneumonia (with major complications)"
+      },
+      {
+        "code": "191",
+        "name": "COPD (with complications)"
+      },
+      {
+        "code": "207",
+        "name": "Respiratory system diagnosis with ventilator support 96+ Hours"
+      },
+      {
+        "code": "208",
+        "name": "Respiratory system diagnosis with ventilator support <96 Hours"
+      }
+    ]
+  },
+  {
+    "name": "Other",
+    "DRGs": [
+      {
+        "code": "439",
+        "name": "Pancreatic disorders"
+      },
+      {
+        "code": "419",
+        "name": "Removal of the gall bladder"
+      },
+      {
+        "code": "149",
+        "name": "Dizziness"
+      },
+      {
+        "code": "917",
+        "name": "Poisoning (with major complications)"
+      },
+      {
+        "code": "897",
+        "name": "Substance abuse"
+      },
+      {
+        "code": "885",
+        "name": "Psychosis"
+      },
+      {
+        "code": "948",
+        "name": "Pain control and abnormal findings"
+      },
+      {
+        "code": "918",
+        "name": "Poisoning"
+      },
+      {
+        "code": "418",
+        "name": "Removal of the gall bladder (with complications)"
+      }
+    ]
+  }
+]
